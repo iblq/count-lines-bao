77f8b91b0f35749af3ae6cf46256144a79cd8f4d
baolq <luoqiang.bao@gmail.com>
2019-06-08 14:48:59 +0800

    update

--- bin/count.js
+++ bin/count.js
@@ -8,7 +8,6 @@ program
   .option('-a, --all', '统计包括子文件夹文件')
   .option('ext, --type [type]', '最后一个参数，请输入文件扩展名，如 js , css,只接受一个参数','js')
   .action(function (env) {
-    console.log(env)
   })
   .parse(process.argv);
 
